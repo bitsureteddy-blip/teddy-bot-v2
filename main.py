7477218cc4cb9c03bc49ddee3653db6e0a134d7d
bitsureteddy-blip <bitsureteddy@gmail.com>
2026-05-03 12:15:51 +0200

    Update menu callback handler pattern for checkdir and subscription

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main():
         app.add_handler(CommandHandler(cmd, func))
 
     # Callbacks
-    app.add_handler(CallbackQueryHandler(menu_callback, pattern="^(menu_|cmd_)"))
+    app.add_handler(CallbackQueryHandler(menu_callback, pattern="^(menu_|cmd_|checkdir_|check_subscription)"))
     app.add_handler(CallbackQueryHandler(symbol_callback, pattern="^(symcat_|sympage_|symsel_|noop)"))
     app.add_handler(CallbackQueryHandler(clearalerts_callback, pattern="^clearalerts_"))
     app.add_handler(CallbackQueryHandler(plan_callback, pattern="^plan_"))
