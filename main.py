792086a0cfdd9880bb9acbdad91efe5d5559583f
bitsureteddy-blip <bitsureteddy@gmail.com>
2026-05-16 17:53:42 +0200

    Désactiver alert monitoring + signal monitoring - stop hémorragie API

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
     AlertManager.get_instance()
 
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    AlertManager.get_instance().start_monitoring(app)
+  # AlertManager.get_instance().start_monitoring(app)
     start_weekly_report_scheduler(app)
   # start_signal_monitoring(app)
 
